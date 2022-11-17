0f48ca72c0b07be794172b63c2dcff4a8da0f955
Lucas Ferreira <lucas.ferreira@quatainvestimentos.com.br>
2022-11-17 11:37:29 -0300

    feat- fistComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
- return(<h1>Nada</h1>)
+  return (
+    <>
+      <h1>Nada</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
