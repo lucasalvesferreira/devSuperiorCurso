70deaf7b5afb8481a1a018b30b863e67a686a893
unknown <lucas.aferreira@bandtec.com.br>
2022-11-20 21:35:13 -0300

    feat- send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,26 @@
+import axios from 'axios';
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
 
-function NotificationButton() {
+
+type Props = {
+        saleId: number;
+}
+function handreClick(id :number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response =>{
+
+    });
+
+}
+
+function NotificationButton({saleId} : Props ) {
     return (
-        <div className="dsmeta-red-btn">
-                          <img src={icon} alt="Notificar" />
-                        </div>
+        <div className="dsmeta-red-btn" onClick={() => handreClick(saleId)}>
+            <img src={icon} alt="Notificar" />
+        </div>
     )
 }
 
